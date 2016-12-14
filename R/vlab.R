0e377370e937cecb0eb656afdfbe68c9a4410b5f
ptagliolato <ptagliolato@users.noreply.github.com>
2016-12-14 11:57:44 +0100

    minor changes

--- R/vlab.R
+++ R/vlab.R
@@ -2,6 +2,10 @@ a<-function(s1){
   return (c(s1,": this is almost a test!"))  
 }
 #
+
+# The method returns the first row of the downloaded dataset
+# In openCPU, when the request is invoked with POST method, the dataset can then be found in /ocpu/tmp/{key}/files/lw_{dataUID}
+# The session {key} is given in X-ocpu-session http response header returned by openCPU
 tavReadDataportal<-function(dataUID,user,pass){
   require(curl)
   h <- new_handle()
@@ -19,7 +23,7 @@ tavReadDataportal<-function(dataUID,user,pass){
   req<-curl_download(url = url,sprintf("%s/lw_%s",getwd(),dataUID),handle=h)
   
   # extract just the header (colnames)
-  header <- read.table(req, nrows = 2, header = FALSE, sep =';', stringsAsFactors = FALSE)
+  header <- read.table(req, nrows = 1, header = TRUE, sep =';', stringsAsFactors = FALSE)
   
-  return(header)#test
+  return(header)
 }
