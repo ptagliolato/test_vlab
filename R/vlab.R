560704461aaadef17c8002ce0c5626ac9cb2dc49
ptagliolato <ptagliolato@users.noreply.github.com>
2016-12-15 16:11:04 +0100

    documentation

--- R/vlab.R
+++ R/vlab.R
@@ -1,11 +1,25 @@
+#' test function
+#'
+#' @param s1 
 a<-function(s1){
   return (c(s1,": this is almost a test!"))  
 }
 #
 
-# The method returns the first row of the downloaded dataset
-# In openCPU, when the request is invoked with POST method, the dataset can then be found in /ocpu/tmp/{key}/files/lw_{dataUID}
-# The session {key} is given in X-ocpu-session http response header returned by openCPU
+#' Download a dataset from LifeWatch Service Centre dataportal
+#' 
+#' \code{tavReadDataportal} The method returns the first row of the downloaded dataset
+#' In openCPU, when the request is invoked with POST method, the dataset can then be found in /ocpu/tmp/{key}/files/lw_{dataUID}
+#' The session {key} is given in "X-ocpu-session" http response header returned by openCPU. 
+#' Alternatively, the complete path to session folder ({baseurl}/ocpu/tmp/{key}/) is given in "Location" header
+#'
+#' @param dataUID unique identifier of the dataset, retrievable through dataportal API or search interface
+#' @param user username (LW dataportal account)
+#' @param pass password (LW dataportal account)
+#' @import curl
+#'
+#' @export
+#' @return first row of the downloaded dataset
 tavReadDataportal<-function(dataUID,user,pass){
   require(curl)
   h <- new_handle()
@@ -22,7 +36,7 @@ tavReadDataportal<-function(dataUID,user,pass){
   # cf. https://github.com/jeroenooms/opencpu/issues/175 "Writing to a unique temporary directory": in openCPU use the getwd dir.
   req<-curl_download(url = url,sprintf("%s/lw_%s",getwd(),dataUID),handle=h)
   
-  # extract just the header (colnames)
+  # extract just the first row with colnames as an example
   header <- read.table(req, nrows = 1, header = TRUE, sep =';', stringsAsFactors = FALSE)
   
   return(header)
